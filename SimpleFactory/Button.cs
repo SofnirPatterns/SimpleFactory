89a1ba7971be775b7fcc46d524ea13592e05f584
macie <macie@DESKTOP-GUK2RJ2>
2021-04-11 20:45:13 +0200

    run on vs 2019

--- SimpleFactory/Button.cs
+++ SimpleFactory/Button.cs
@@ -7,7 +7,7 @@ namespace SimpleFactory
     public abstract class Button
     {
         public string Text { get; set; }
-        private StringBuilder ButtonWidget= new StringBuilder();
+        private StringBuilder ButtonWidget = new StringBuilder();
         public ConsoleKey ConsoleKey { get; set; }
 
         public Button(string text, ConsoleKey consoleKey)
